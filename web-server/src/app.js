a369f5d1cd39a3c2c3a83d46245dd48f11768757
Yonas Yeneneh <yonasyeneneh@Yonass-MBP.phub.net.cable.rogers.com>
2020-07-12 21:59:11 -0400

    change port to 3000

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,12 @@
 const express =  require('express')
 
 const app = express()
+const PORT = 3000
 
 app.get('', (req, res)=>{
     res.send('<h1>Hello Yonas</h1><br> Dont worry you can make it!')
 })
 
-app.listen(4000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Server started on port 3000')
 })
