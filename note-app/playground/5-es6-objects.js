13052003257f60a295aebc8a3e942159bf97ec21
sanoylab <expertsanoy@gmail.com>
2020-07-12 22:43:22 -0400

    removing the node_module folder

--- note-app/playground/5-es6-objects.js
+++ note-app/playground/5-es6-objects.js
@@ -17,3 +17,16 @@ const product = {
 
 const {price} = product
 console.log(price+1)
+
+
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+app.get('/',(req, res)=>{
+  res.send('Hello world')
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server is up and running..')
+})
